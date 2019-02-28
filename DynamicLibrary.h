06af4cf6744956fcf60c9053e50d9298ebb03157
sty <scarsty@gmail.com>
2019-02-28 20:44:00 +0800

    no message

--- DynamicLibrary.h
+++ DynamicLibrary.h
@@ -23,5 +23,5 @@ private:
 
 public:
     static void* loadDynamicLibrary(std::string library_name);
-    static void* getFunction(std::string library_name, std::string function_name);
+    static void* getFunction(const std::string& library_name, const std::string& function_name);
 };
