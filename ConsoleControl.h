ecf93859e59f5f77dc0669285de041e0a8c80511
sty <scarsty@gmail.com>
2019-03-30 12:56:31 +0800

    consolecontrol 和 DynamicLibrary 改为只用头文件

--- ConsoleControl.h
+++ ConsoleControl.h
@@ -1,6 +1,13 @@
 #pragma once
 #include <map>
+#include <stdio.h>
 #include <string>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+
+#endif
 
 enum ConsoleColor
 {
@@ -18,8 +25,32 @@ enum ConsoleColor
 class ConsoleControl
 {
 private:
-    ConsoleControl();
-    virtual ~ConsoleControl();
+    ConsoleControl()
+    {
+        if (color_map_.empty())
+        {
+            color_map_ =
+            {
+                { CONSOLE_COLOR_NONE, "\e[0m" },
+                { CONSOLE_COLOR_RED, "\e[0;31m" },
+                { CONSOLE_COLOR_LIGHT_RED, "\e[1;31m" },
+                { CONSOLE_COLOR_GREEN, "\e[0;32m" },
+                { CONSOLE_COLOR_LIGHT_GREEN, "\e[1;32m" },
+                { CONSOLE_COLOR_BLUE, "\e[0;34m" },
+                { CONSOLE_COLOR_LIGHT_BLUE, "\e[1;34m" },
+                { CONSOLE_COLOR_WHITE, "\e[1;37m" },
+                { CONSOLE_COLOR_BLACK, "\e[0;30m" },
+            };
+#ifdef _WIN32
+            CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+            GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbiInfo);
+            old_color_ = csbiInfo.wAttributes;
+#endif
+        }
+    }
+    virtual ~ConsoleControl()
+    {
+    }
 
     unsigned short old_color_;
     std::map<int, std::string> color_map_;
@@ -35,6 +66,31 @@ private:
     ConsoleControl& operator=(ConsoleControl&) = delete;
 
 public:
-    static void setColor(int c);
-    static void moveUp(int l = 1);
+    static void setColor(int c)
+    {
+        auto cc = getInstance();
+#ifdef _MSC_VER
+        if (c != CONSOLE_COLOR_NONE)
+        {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), c);
+        }
+        else
+        {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), cc->old_color_);
+        }
+#else
+        fprintf(stderr, cc->color_map_[c].c_str());
+#endif
+    }
+    static void moveUp(int l = 1)
+    {
+#ifdef _MSC_VER
+        CONSOLE_SCREEN_BUFFER_INFO info;
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+        info.dwCursorPosition.Y -= l;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), info.dwCursorPosition);
+#else
+        fprintf(stderr, "\e[%dA", l);
+#endif
+    }
 };
--- DynamicLibrary.h
+++ DynamicLibrary.h
@@ -1,6 +1,14 @@
 #pragma once
+#include <fstream>
 #include <map>
 #include <string>
+
+#ifdef _WIN32
+#define NOMINMAX
+#include <windows.h>
+#else
+#include <dlfcn.h>
+#endif
 
 class DynamicLibrary
 {
@@ -15,13 +23,65 @@ private:
     DynamicLibrary& operator=(DynamicLibrary&) = delete;
 
 private:
-    DynamicLibrary();
-    ~DynamicLibrary();
+    DynamicLibrary()
+    {
+    }
+    //free loaded dynamic libraries automatically when destruction
+    ~DynamicLibrary()
+    {
+        for (auto dl : dynamic_libraries_)
+        {
+#ifdef _WIN32
+            FreeLibrary((HINSTANCE)dl.second);
+#else
+            dlclose(dl.second);
+#endif
+        }
+    }
 
 private:
     std::map<std::string, void*> dynamic_libraries_;
 
 public:
-    static void* loadDynamicLibrary(std::string library_name);
-    static void* getFunction(const std::string& library_name, const std::string& function_name);
+    static void* loadDynamicLibrary(const std::string& library_name)
+    {
+        auto dl = getInstance();
+        if (dl->dynamic_libraries_.count(library_name) <= 0)
+        {
+            void* hlib = nullptr;
+#ifdef _WIN32
+            hlib = LoadLibraryA(library_name.c_str());
+#else
+            hlib = dlopen(library_name.c_str(), RTLD_LAZY);
+#endif
+            dl->dynamic_libraries_[library_name] = hlib;
+            if (hlib)
+            {
+                fprintf(stdout, "Loaded dynamic library: %s\n", library_name.c_str());
+            }
+			else
+			{
+				fprintf(stdout, "Failed to load dynamic library: %s\n", library_name.c_str());
+			}
+        }
+        return dl->dynamic_libraries_[library_name];
+    }
+    static void* getFunction(const std::string& library_name, const std::string& function_name)
+    {
+        auto dl = getInstance()->loadDynamicLibrary(library_name);
+        void* func = nullptr;
+        if (dl)
+        {
+#ifdef _WIN32
+            func = GetProcAddress((HINSTANCE)dl, function_name.c_str());
+#else
+            func = dlsym(dl, function_name.c_str());
+#endif
+        }
+		else
+		{
+			fprintf(stdout, "Failed to load function %s\n", function_name.c_str());
+		}
+        return func;
+    }
 };
