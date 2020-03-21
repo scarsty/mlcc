b653332e9da8bc3b7d0f62ec4f9bc047fc760248
sty <scarsty@gmail.com>
2020-03-22 00:59:47 +0800

    two class to namespace

--- File.h
+++ File.h
@@ -3,72 +3,63 @@
 #include <string>
 #include <vector>
 
-class File
+namespace File
 {
-private:
-    File();
-    virtual ~File();
 
-public:
-    //read and write file
-    static bool fileExist(const std::string& filename);
-    static void reverse(char* c, int n);
+//read and write file
+bool fileExist(const std::string& filename);
+void reverse(char* c, int n);
 
-    static std::vector<char> readFile(const std::string& filename, int length = -1);
-    static int readFile(const std::string& filename, void* s, int length);
-    static int writeFile(const std::string& filename, void* s, int length);
+std::vector<char> readFile(const std::string& filename, int length = -1);
+int readFile(const std::string& filename, void* s, int length);
+int writeFile(const std::string& filename, void* s, int length);
 
-    template <class T>
-    static void readDataToVector(const char* data, int length, std::vector<T>& v)
+template <class T>
+void readDataToVector(const char* data, int length, std::vector<T>& v, int length_one)
+{
+    int count = length / length_one;
+    v.resize(count);
+    for (int i = 0; i < count; i++)
     {
-        readDataToVector(data, length, v, sizeof(T));
+        memcpy(&v[i], data + length_one * i, length_one);
     }
+}
 
-    template <class T>
-    static void readDataToVector(const char* data, int length, std::vector<T>& v, int length_one)
+template <class T>
+void readDataToVector(const char* data, int length, std::vector<T>& v)
+{
+    readDataToVector(data, length, v, sizeof(T));
+}
+
+template <class T>
+void readFileToVector(const std::string& filename, std::vector<T>& v)
+{
+    auto buffer = readFile(filename);
+    readDataToVector(buffer.data(), buffer.size(), v);
+}
+
+template <class T>
+void writeVectorToData(char* data, int length, std::vector<T>& v, int length_one)
+{
+    int count = length / length_one;
+    v.resize(count);
+    for (int i = 0; i < count; i++)
     {
-        int count = length / length_one;
-        v.resize(count);
-        for (int i = 0; i < count; i++)
-        {
-            memcpy(&v[i], data + length_one * i, length_one);
-        }
+        memcpy(data + length_one * i, &v[i], length_one);
     }
+}
 
-    template <class T>
-    static void readFileToVector(const std::string& filename, std::vector<T>& v)
-    {
-        auto buffer = readFile(filename);
-        readDataToVector(buffer.data(), buffer.size(), v);
-    }
+//other file operations
+std::vector<std::string> getFilesInPath(const std::string& path_name, int recursive = 0, int include_path = 0);
+bool isPath(const std::string& name);
+std::string getFileTime(std::string filename);
+void changePath(const std::string& path);
 
-    template <class T>
-    static void writeVectorToData(char* data, int length, std::vector<T>& v, int length_one)
-    {
-        int count = length / length_one;
-        v.resize(count);
-        for (int i = 0; i < count; i++)
-        {
-            memcpy(data + length_one * i, &v[i], length_one);
-        }
-    }
+//functions about file name
+std::string getFileExt(const std::string& filename);
+std::string getFileMainname(const std::string& fileName);
+std::string getFilenameWithoutPath(const std::string& fileName);
+std::string changeFileExt(const std::string& filename, const std::string& ext);
+std::string getFilePath(const std::string& filename);
 
-    //other file operations
-    static std::vector<std::string> getFilesInPath(const std::string& path_name, int recursive = 0, int include_path = 0);
-    static bool isPath(const std::string& name);
-    static std::string getFileTime(std::string filename);
-    static void changePath(const std::string& path);
-
-    //functions about filename
-
-private:
-    static size_t getLastPathCharPos(const std::string& filename);
-
-public:
-    //functions about file name
-    static std::string getFileExt(const std::string& filename);
-    static std::string getFileMainname(const std::string& fileName);
-    static std::string getFilenameWithoutPath(const std::string& fileName);
-    static std::string changeFileExt(const std::string& filename, const std::string& ext);
-    static std::string getFilePath(const std::string& filename);
-};
+}    // namespace File
