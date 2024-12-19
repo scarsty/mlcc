7ed76c1dd0062f0c3878010155118b451063bc7a
sty <scarsty@gmail.com>
2024-12-19 23:52:42 +0800

    分开声明和实现

---
+++ vramusage.cpp
@@ -0,0 +1,156 @@
+#include "vramusage.h"
+
+#include <cfgmgr32.h>
+#include <d3dkmthk.h>
+#include <devpkey.h>
+#include <tchar.h>
+
+#include <cstdint>
+#include <cstdio>
+
+#pragma comment(lib, "cfgmgr32.lib")
+#pragma comment(lib, "Gdi32.lib")
+#pragma comment(lib, "kernel32.lib")    //for HOST Memory(RAM)
+
+std::vector<GPUDeviceLUID> get_gpu_devices_luid()
+{
+    std::vector<GPUDeviceLUID> devices;
+    LUID luid{ 0, 0 };
+
+    PWCHAR deviceInterfaceList;
+    ULONG deviceInterfaceListLength = 0;
+    PWCHAR deviceInterface;
+    if (CM_Get_Device_Interface_List_Size(&deviceInterfaceListLength, (GUID*)&GUID_DISPLAY_DEVICE_ARRIVAL, NULL, CM_GET_DEVICE_INTERFACE_LIST_PRESENT)) { return devices; }
+    std::vector<WCHAR> deviceInterfaceListBuffer(deviceInterfaceListLength);
+    deviceInterfaceList = deviceInterfaceListBuffer.data();
+
+    if (CM_Get_Device_Interface_List((GUID*)&GUID_DISPLAY_DEVICE_ARRIVAL, NULL, deviceInterfaceList, deviceInterfaceListLength, CM_GET_DEVICE_INTERFACE_LIST_PRESENT)) { return devices; }
+
+    auto p = deviceInterfaceList;
+    while (*p != L'\0')
+    {
+        deviceInterface = p;
+        p += wcslen(p) + 1;
+#ifdef _DEBUG
+        wprintf(L"%s\n", deviceInterface);
+#endif
+
+        DEVPROPTYPE devicePropertyType;
+        DEVINST deviceInstanceHandle;
+        ULONG deviceInstanceIdLength = MAX_DEVICE_ID_LEN;
+        WCHAR deviceInstanceId[MAX_DEVICE_ID_LEN];
+
+        if (CM_Get_Device_Interface_Property(deviceInterface, &DEVPKEY_Device_InstanceId, &devicePropertyType, (PBYTE)deviceInstanceId, &deviceInstanceIdLength, 0)) { continue; }
+        if (CM_Locate_DevNode(&deviceInstanceHandle, deviceInstanceId, CM_LOCATE_DEVNODE_NORMAL)) { continue; }
+
+        ULONG buffer;
+        ULONG bufferSize = sizeof(ULONG);
+        DEVPROPTYPE propertyType = DEVPROP_TYPE_EMPTY;
+
+        if (CM_Get_DevNode_Property(deviceInstanceHandle, &DEVPKEY_Device_BusNumber, &propertyType, (PBYTE)&buffer, &bufferSize, 0)) { continue; }
+
+        D3DKMT_OPENADAPTERFROMDEVICENAME openAdapterFromDeviceName{};
+        openAdapterFromDeviceName.pDeviceName = deviceInterface;
+        D3DKMTOpenAdapterFromDeviceName(&openAdapterFromDeviceName);
+        D3DKMT_CLOSEADAPTER closeAdapter;
+        closeAdapter.hAdapter = openAdapterFromDeviceName.hAdapter;
+        D3DKMTCloseAdapter(&closeAdapter);
+        luid = openAdapterFromDeviceName.AdapterLuid;
+        GPUDeviceLUID device;
+
+        device.luid = luid;
+        device.pcibus = buffer;
+        devices.push_back(device);
+    }
+    return devices;
+}
+
+LUID get_luid_from_pcibus(int pcibus)
+{
+    LUID luid{ 0, 0 };
+    auto devices = get_gpu_devices_luid();
+    for (auto& device : devices)
+    {
+        if (device.pcibus == pcibus)
+        {
+            luid = device.luid;
+            break;
+        }
+    }
+    return luid;
+}
+
+int get_free_mem_by_luid(LUID luid, size_t* physical, size_t* shared)
+{
+    D3DKMT_QUERYSTATISTICS queryStatistics{};
+    queryStatistics.Type = D3DKMT_QUERYSTATISTICS_ADAPTER;
+    queryStatistics.AdapterLuid = luid;
+    if (D3DKMTQueryStatistics(&queryStatistics))
+    {
+        //printf("D3DKMTQueryStatistics failed with %d\n", ret);
+        return 1;
+    }
+
+    ULONG64 total = 0, shared_usage = 0, physical_usage = 0;
+    for (int i = 0; i < queryStatistics.QueryResult.AdapterInformation.NbSegments; i++)
+    {
+        D3DKMT_QUERYSTATISTICS queryStatistics2{};
+        queryStatistics2.Type = D3DKMT_QUERYSTATISTICS_SEGMENT;
+        queryStatistics2.AdapterLuid = luid;
+        queryStatistics2.QuerySegment.SegmentId = i;
+        if (D3DKMTQueryStatistics(&queryStatistics2) == 0)
+        {
+            auto commit_limit = queryStatistics2.QueryResult.SegmentInformation.BytesResident;
+            auto aperture = queryStatistics2.QueryResult.SegmentInformation.Aperture;
+            if (aperture)
+            {
+                shared_usage += commit_limit;
+            }
+            else
+            {
+                physical_usage += commit_limit;
+            }
+            total += commit_limit;
+        }
+    }
+    //printf("%g %g\n", sharedUsage / pow(2, 20), dedicatedUsage / pow(2, 20));
+    if (physical) { *physical = physical_usage; }
+    if (shared) { *shared = shared_usage; }
+    return 0;
+}
+
+float get_temperature_by_luid(LUID luid)
+{
+    D3DKMT_QUERYSTATISTICS queryStatistics{};
+    queryStatistics.Type = D3DKMT_QUERYSTATISTICS_PHYSICAL_ADAPTER;
+    queryStatistics.AdapterLuid = luid;
+    if (D3DKMTQueryStatistics(&queryStatistics))
+    {
+        //printf("D3DKMTQueryStatistics failed with %d\n", ret);
+        return 1;
+    }
+    return queryStatistics.QueryResult.PhysAdapterInformation.AdapterPerfData.Temperature / 10.0;
+}
+
+int get_free_mem_by_pcibus(int pcibus, size_t* resident, size_t* shared)
+{
+    auto luid = get_luid_from_pcibus(pcibus);
+    return get_free_mem_by_luid(luid, resident, shared);
+}
+
+bool get_free_host_mem(size_t* totalMemory, size_t* freeMemory, size_t* totalVirtualMemory, size_t* freeVirtualMemory)
+{
+    MEMORYSTATUSEX memoryInfo;
+    memoryInfo.dwLength = sizeof(memoryInfo);
+    if (!GlobalMemoryStatusEx(&memoryInfo))
+    {
+        return false;
+    }
+
+    if (totalMemory) { *totalMemory = memoryInfo.ullTotalPhys; }
+    if (freeMemory) { *freeMemory = memoryInfo.ullAvailPhys; }
+    if (totalVirtualMemory) { *totalVirtualMemory = memoryInfo.ullTotalVirtual; }
+    if (freeVirtualMemory) { *freeVirtualMemory = memoryInfo.ullAvailVirtual; }
+
+    return true;
+}
--- vramusage.h
+++ vramusage.h
@@ -1,152 +1,38 @@
-#pragma once
+﻿#pragma once
 
+#include <vector>
+
+// the turn of the next 4 lines cannot be changed
 #define INITGUID
 #include <objbase.h>
 
-#include <cfgmgr32.h>
-#include <devpkey.h>
 #include <ntddvdeo.h>
-#include <windows.h>
 
-#include <cstdio>
-#include <d3dkmthk.h>
-#include <tchar.h>
+#include <windows.h>    //after ntddvdeo.h
 
-#include <cstdint>
-#include <vector>
+struct GPUDeviceLUID
+{
+    LUID luid;
+    int pcibus;
+};
 
-#pragma comment(lib, "cfgmgr32.lib")
-#pragma comment(lib, "Gdi32.lib")
-#pragma comment(lib, "kernel32.lib")    //for HOST Memory(RAM)
+// Get GPU Devices
+// PCIBus is the bus number of the GPU device, if it is 0, it may be a virtual device
+std::vector<GPUDeviceLUID> get_gpu_devices_luid();
+
+// Get LUID from PCIBus, only for GPU devices
+LUID get_luid_from_pcibus(int pcibus);
 
 // GPU Device Memory(VRAM)
-inline LUID get_luid_from_pcibus(int pcibus)
-{
-    LUID luid{ 0, 0 };
-
-    PWCHAR deviceInterfaceList;
-    ULONG deviceInterfaceListLength = 0;
-    PWCHAR deviceInterface;
-    if (CM_Get_Device_Interface_List_Size(&deviceInterfaceListLength, (GUID*)&GUID_DISPLAY_DEVICE_ARRIVAL, NULL, CM_GET_DEVICE_INTERFACE_LIST_PRESENT)) { return luid; }
-    std::vector<WCHAR> deviceInterfaceListBuffer(deviceInterfaceListLength);
-    deviceInterfaceList = deviceInterfaceListBuffer.data();
-
-    if (CM_Get_Device_Interface_List((GUID*)&GUID_DISPLAY_DEVICE_ARRIVAL, NULL, deviceInterfaceList, deviceInterfaceListLength, CM_GET_DEVICE_INTERFACE_LIST_PRESENT)) { return luid; }
-
-    auto p = deviceInterfaceList;
-    while (*p != L'\0')
-    {
-        deviceInterface = p;
-        p += wcslen(p) + 1;
-#ifdef _DEBUG
-        //wprintf(L"%s\n", deviceInterface);
-#endif
-
-        DEVPROPTYPE devicePropertyType;
-        DEVINST deviceInstanceHandle;
-        ULONG deviceInstanceIdLength = MAX_DEVICE_ID_LEN;
-        WCHAR deviceInstanceId[MAX_DEVICE_ID_LEN];
-
-        if (CM_Get_Device_Interface_Property(deviceInterface, &DEVPKEY_Device_InstanceId, &devicePropertyType, (PBYTE)deviceInstanceId, &deviceInstanceIdLength, 0)) { continue; }
-        if (CM_Locate_DevNode(&deviceInstanceHandle, deviceInstanceId, CM_LOCATE_DEVNODE_NORMAL)) { continue; }
-
-        ULONG buffer;
-        ULONG bufferSize = sizeof(ULONG);
-        DEVPROPTYPE propertyType = DEVPROP_TYPE_EMPTY;
-
-        if (CM_Get_DevNode_Property(deviceInstanceHandle, &DEVPKEY_Device_BusNumber, &propertyType, (PBYTE)&buffer, &bufferSize, 0)) { continue; }
-
-        if (buffer == pcibus)
-        {
-            D3DKMT_OPENADAPTERFROMDEVICENAME openAdapterFromDeviceName{};
-            openAdapterFromDeviceName.pDeviceName = deviceInterface;
-            D3DKMTOpenAdapterFromDeviceName(&openAdapterFromDeviceName);
-            D3DKMT_CLOSEADAPTER closeAdapter;
-            closeAdapter.hAdapter = openAdapterFromDeviceName.hAdapter;
-            D3DKMTCloseAdapter(&closeAdapter);
-            luid = openAdapterFromDeviceName.AdapterLuid;
-            break;
-        }
-    }
-
-    return luid;
-}
-
-// GPU Device Memory(VRAM)
-inline int get_free_mem_by_luid(LUID luid, size_t* resident, size_t* shared)
-{
-    D3DKMT_QUERYSTATISTICS queryStatistics{};
-    queryStatistics.Type = D3DKMT_QUERYSTATISTICS_ADAPTER;
-    queryStatistics.AdapterLuid = luid;
-    if (D3DKMTQueryStatistics(&queryStatistics))
-    {
-        //printf("D3DKMTQueryStatistics failed with %d\n", ret);
-        return 1;
-    }
-
-    ULONG64 total = 0, sharedUsage = 0, residendUsage = 0;
-    for (int i = 0; i < queryStatistics.QueryResult.AdapterInformation.NbSegments; i++)
-    {
-        D3DKMT_QUERYSTATISTICS queryStatistics2{};
-        queryStatistics2.Type = D3DKMT_QUERYSTATISTICS_SEGMENT;
-        queryStatistics2.AdapterLuid = luid;
-        queryStatistics2.QuerySegment.SegmentId = i;
-        if (D3DKMTQueryStatistics(&queryStatistics2) == 0)
-        {
-            auto commitLimit = queryStatistics2.QueryResult.SegmentInformation.BytesResident;
-            auto aperture = queryStatistics2.QueryResult.SegmentInformation.Aperture;
-            if (aperture)
-            {
-                sharedUsage += commitLimit;
-            }
-            else
-            {
-                residendUsage += commitLimit;
-            }
-            total += commitLimit;
-        }
-    }
-    //printf("%g %g\n", sharedUsage / pow(2, 20), dedicatedUsage / pow(2, 20));
-    if (resident) { *resident = residendUsage; }
-    if (shared) { *shared = sharedUsage; }
-    return 0;
-}
+// physical means the memory that is dedicated to the GPU, shared means the memory that is shared with the system
+// physical为硬件上的显存占用，shared为系统共享显存的占用
+int get_free_mem_by_luid(LUID luid, size_t* physical, size_t* shared);
 
 // GPU Temperature
-inline float get_temperature_by_luid(LUID luid)
-{
-    D3DKMT_QUERYSTATISTICS queryStatistics{};
-    queryStatistics.Type = D3DKMT_QUERYSTATISTICS_PHYSICAL_ADAPTER;
-    queryStatistics.AdapterLuid = luid;
-    if (D3DKMTQueryStatistics(&queryStatistics))
-    {
-        //printf("D3DKMTQueryStatistics failed with %d\n", ret);
-        return 1;
-    }
-    return queryStatistics.QueryResult.PhysAdapterInformation.AdapterPerfData.Temperature / 10.0;
-}
+float get_temperature_by_luid(LUID luid);
 
-// GPU Device Memory(VRAM)
-inline int get_free_mem_by_pcibus(int pcibus, size_t* resident, size_t* shared)
-{
-    auto luid = get_luid_from_pcibus(pcibus);
-    return get_free_mem_by_luid(luid, resident, shared);
-}
+// GPU Device Memory Usage by PCIBus
+int get_free_mem_by_pcibus(int pcibus, size_t* resident, size_t* shared);
 
 // CPU Host Memory(RAM)
-inline bool get_free_host_mem(size_t* totalMemory, size_t* freeMemory, size_t* totalVirtualMemory, size_t* freeVirtualMemory)
-{
-    MEMORYSTATUSEX memoryInfo;
-    memoryInfo.dwLength = sizeof(memoryInfo);
-    if (!GlobalMemoryStatusEx(&memoryInfo))
-    {
-        return false;
-    }
-
-    if (totalMemory) { *totalMemory = memoryInfo.ullTotalPhys; }
-    if (freeMemory) { *freeMemory = memoryInfo.ullAvailPhys; }
-    if (totalVirtualMemory) { *totalVirtualMemory = memoryInfo.ullTotalVirtual; }
-    if (freeVirtualMemory) { *freeVirtualMemory = memoryInfo.ullAvailVirtual; }
-
-    return true;
-}
+bool get_free_host_mem(size_t* totalMemory, size_t* freeMemory, size_t* totalVirtualMemory, size_t* freeVirtualMemory);
